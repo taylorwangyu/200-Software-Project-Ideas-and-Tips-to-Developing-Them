82842430c3dbcce5e689808d914936fceecce0eb
taylorwangyu <Taylor@TaylortekiMacBook-Pro.local>
2016-03-14 03:32:26 +0800

    test
    test

---
+++ Numbers_1_Fractions.py
@@ -0,0 +1,6 @@
+# Numbers problem 1
+# Description: 
+# This project involves working with fractions. How do you add 1⁄3 + 1⁄5? Create a program that 
+# first asks the user which operation they want to do: add, subtract, multiply or divide and 
+# then asks for 1, 2 or more fractions to work with. The program prints out the result.
+
